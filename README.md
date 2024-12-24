c3db993fbfc268ba4f44f387b7be429f5803019e
Andrzej Wojciechowski <AWojciechowski93@gmail.com>
2024-12-24 10:36:53 +0100

    Added unit inversion

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # alfred-invert-SI
 Calculate the inverse of a value with SI prefix
+The script automatically inverts the following units (both ways):
+- second <-> hertz
+- ohm    <-> siemens
+In other cases the '⁻¹' suffix is added to the supplied unit
 
 Example:
 - from 50MHz (frequency) calculate 20ns (period)
@@ -12,7 +16,8 @@ Example:
 For example:
 - `inv 60`     -> 16.667m
 - `inv 20u`    -> 50k
-- `inv 50 MHz` -> 20 nHz⁻¹
+- `inv 50 MHz` -> 20 ns
+- `inv 50 m`   -> 20 m⁻¹
 
 ## Configuration
 
--- workflow/script.py
+++ workflow/script.py
@@ -2,13 +2,23 @@ import sys
 import json
 from quantiphy import Quantity
 
+inv_units = {
+   "Hz" : "s",
+   "hz" : "s",
+   "s"  : "Hz",
+   "Ω"  : "S",
+   "ohm": "S",
+   "Ohm": "S",
+   "S"  : "Ω"
+}
 
 try:
    iv = Quantity(sys.argv[1])
    if iv.units == '':
       conv_result = Quantity(1.0/iv).render()
    else:
-      conv_result = Quantity(1.0/iv, iv.units + "⁻¹").render()
+      unit = inv_units.get(iv.units, iv.units + "⁻¹")
+      conv_result = Quantity(1.0/iv, unit).render()
 except Exception as e:
    conv_result = 'Invalid value'
 
